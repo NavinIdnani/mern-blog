febf0975bf6df82954901fca58dee93af7c03793
NavinIdnani <NavinIdnani.idnaninavin5202@gmail.com>
2025-02-13 16:41:02 +0530

    first commit

---
+++ client/src/components/about/About.jsx
@@ -0,0 +1,54 @@
+import { Box, styled, Typography, Link } from '@mui/material';
+import { GitHub, Instagram, Email } from '@mui/icons-material';
+
+const Banner = styled(Box)`
+    background-image: url(https://img.freepik.com/free-photo/close-up-image-programer-working-his-desk-office_1098-18707.jpg?t=st=1739302926~exp=1739306526~hmac=a3628efccf70f9d62bb0f30d0e17f56a32c348c8b4837a46653c08db25df3875&w=900);
+    width: 100%;
+    height: 50vh;
+    background-position: left 0px bottom 0px;
+    background-size: cover;
+`;
+
+const Wrapper = styled(Box)`
+    padding: 20px;
+    & > h3, & > h5 {
+        margin-top: 50px;
+    }
+`;
+
+const Text = styled(Typography)`
+    color: #878787;
+`;
+
+const About = () => {
+
+    return (
+        <Box>
+            <Banner/>
+            <Wrapper>
+                <Typography variant="h3">Navin Idnani</Typography>
+                <Text variant="h5">I'm a Software Engineer based in India. 
+                    I've built websites, desktop applications and corporate software.<br />
+                    If you are interested, you can view some of my favorite projects here
+                    <Box component="span" style={{ marginLeft: 5 }}>
+                        <Link href="https://github.com/NavinIdnani" color="inherit" target="_blank"><GitHub /></Link>
+                    </Box>
+                </Text>
+                <Text variant="h5">
+                    Need something built or simply want to have chat? Reach out to me on
+                    <Box component="span" style={{ marginLeft: 5 }}>
+                        <Link href="https://www.instagram.com/navin__idnani/" color="inherit" target="_blank">
+                            <Instagram />
+                        </Link>
+                    </Box>  
+                        or send me an Email 
+                        <Link href="mailto:idnaninavin5202@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
+                            <Email />
+                        </Link>.
+                </Text>
+            </Wrapper>
+        </Box>
+    )
+}
+
+export default About;
---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import Connection from './database/db.js';
+import Router from './routes/route.js';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+dotenv.config();
+const __filename=fileURLToPath(import.meta.url);
+const __dirname =path.dirname(__filename);
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.use(cors({
+   origin: process.env.CLIENT_URL || '*'
+}));
+app.use(bodyParser.json({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use('/',Router);
+
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'/client/dist')));
+}
+
+app.get('*',(req,res)=> res.sendFile(path.join(__dirname,'/client/dist/index.html')));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,()=>console.log(`Server is running successfully on PORT ${PORT}`));
+
+const USERNAME=process.env.DB_USERNAME;
+const PASSWORD=process.env.DB_PASSWORD;
+
+const URL=process.env.MONGODB_URI || `mongodb+srv://${USERNAME}:${PASSWORD}@blog-app.avvgj.mongodb.net/`;
+Connection(URL);
