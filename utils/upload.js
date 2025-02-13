ba8063c9856ed2505fa594899d91c95036d1637b
NavinIdnani <97665322+NavinIdnani@users.noreply.github.com>
2025-02-13 19:09:01 +0530

    Update upload.js

--- utils/upload.js
+++ utils/upload.js
@@ -2,6 +2,7 @@ import multer from 'multer';
 
 import { GridFsStorage } from 'multer-gridfs-storage';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -9,7 +10,7 @@ const username=process.env.DB_USERNAME;
 const password= process.env.DB_PASSWORD;
 
 const storage =new GridFsStorage({
-    url:`mongodb+srv://${username}:${password}@blog-app.avvgj.mongodb.net/`,
+    db: mongoose.connection.db,
     // options:{useNewUrlParser:true},
     file:(request,file) =>{
         const match=["image/png","image/jpg"];
