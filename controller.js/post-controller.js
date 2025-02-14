e46d7eed144c39c71f46fa4335e4e69dc821c3e6
NavinIdnani <97665322+NavinIdnani@users.noreply.github.com>
2025-02-15 01:45:23 +0530

    Update post-controller.js

--- controller.js/post-controller.js
+++ controller.js/post-controller.js
@@ -13,44 +13,32 @@ export const createPost =async(request,response)=>{
 }
 
 export const getAllPosts =async(request,response)=>{
+    let username = request.query.username;
     let category=request.query.category;
     let posts;
     try{
-        if(category){
+        if(username) 
+            posts = await Post.find({ username: username });
+        else if(category)
             posts=await Post.find({ categories: category })
-        }else{
+        else
             posts=await Post.find({})
-        }
-    
+        
         return response.status(200).json(posts);
-     }catch(error){
-        return response.status(500).json({msg:Error.message})
+     } catch (error){
+         return response.status(500).json(error)
     }
 }
 
-export const getPost = async (request, response) => {
-  console.log('getPost called with ID:', request.params.id);
-  try {
-    const post = await Post.findById(request.params.id);
-    console.log('Found post:', post); // Check if it's null or an actual document
-    if (!post) return response.status(404).json({ msg: 'Post not found' });
-    return response.status(200).json(post);
-  } catch (error) {
-    console.error('Error in getPost:', error);
-    return response.status(500).json({ msg: error.message });
-  }
-};
-
-
-// export const getPost=async(request,response)=>{
-//     try{
-//         const post=await Post.findById(request.params.id);
-
-//         return response.status(200).json(post);
-//     }catch(error){
-//         return response.status(500).json({msg:error.message});
-//     }
-// }
+export const getPost=async(request,response)=>{
+    try{
+        const post = await Post.findById(request.params.id);
+        
+        return response.status(200).json(post);
+    }catch(error){
+        return response.status(500).json(error);
+    }
+}
 
 export const updatePost=async(request,response)=>{
     try{
