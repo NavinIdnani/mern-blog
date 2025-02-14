a2dcc1d8d234a9c55221d1588afd29ca3fbb22c2
NavinIdnani <97665322+NavinIdnani@users.noreply.github.com>
2025-02-14 16:47:30 +0530

    Update CreatePost.jsx

--- client/src/components/create/CreatePost.jsx
+++ client/src/components/create/CreatePost.jsx
@@ -69,38 +69,23 @@ const CreatePost=()=>{
 
     const url=post.picture ? post.picture : 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
 
-useEffect(() => {
-    setPost((prevPost) => ({
-      ...prevPost,
-      username: account.username,
-      categories: location.search?.split('=')[1] || 'All',
-      createdDate: new Date(), // or keep existing if you prefer
-    }));
-  }, [account.username, location.search]);
 
-  useEffect(() => {
-    const getImage = async () => {
-      if (file) {
-        const data = new FormData();
-        data.append('name', file.name);
-        data.append('file', file);
 
-        try {
-          const response = await API.uploadFile(data);
-          console.log('Upload response:', response);
-          setPost((prevPost) => ({
-            ...prevPost,
-            picture: response.data, // The URL returned by your API
-          }));
-        } catch (error) {
-          console.error('File upload failed:', error);
+ useEffect(() => {
+        const getImage = async () => { 
+            if(file) {
+                const data = new FormData();
+                data.append("name", file.name);
+                data.append("file", file);
+                
+                const response = await API.uploadFile(data);
+                post.picture = response.data;
+            }
         }
-      }
-    };
-    getImage();
-  }, [file]);
-
-
+        getImage();
+        post.categories = location.search?.split('=')[1] || 'All';
+        post.username = account.username;
+    }, [file])
  
 
 
