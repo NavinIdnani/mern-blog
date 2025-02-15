dc250b9c08304b092143c0971a37927f669867b1
NavinIdnani <97665322+NavinIdnani@users.noreply.github.com>
2025-02-15 16:12:07 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { API_NOTIFICATION_MESSAGES ,SERVICE_URLS} from "../constants/config";
 import { getAccessToken,getType } from "../utils/common-utils";
 
-const API_URL='import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+const API_URL= import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 
 const axiosInstance=axios.create({
     baseURL:API_URL,
